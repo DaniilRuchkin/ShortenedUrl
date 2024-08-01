973b7a191a75cf4eec6a24c450be3b89e8235190
данил ручкин <danil-ruchkin01@mail.ru>
2024-08-01 12:17:42 +0300

    Combined class IServiceCollection

--- src/WebAPI/Extentions/InfrastructureExtensions.cs
+++ src/WebAPI/Extentions/InfrastructureExtensions.cs
@@ -1,10 +1,23 @@
-﻿using URLShortener.Application.Interfaces;
+﻿using Microsoft.EntityFrameworkCore;
+using URLShortener.Application.Interfaces;
 using URLShortener.Infrastructure.Services;
+using URLShortener.Persistence.Data;
 
 namespace URLShortener.Web.Extentions;
 
-public static class InfrastructureRedisCacheExtensions
+public static class InfrastructureExtensions
 {
+    public static IServiceCollection AddInfrastructureDbContext(this IServiceCollection services)
+    {
+        var connectionString = Environment.GetEnvironmentVariable("MONGODB_CONNECTION_STRING");
+        var databaseName = Environment.GetEnvironmentVariable("DATABASE_NAME");
+
+        services.AddDbContext<UrlDbContext>(options =>
+            options.UseMongoDB(connectionString!, databaseName!));
+
+        return services;
+    }
+
     public static IServiceCollection AddInfrastructureRedisCacheServices(this IServiceCollection services)
     {
         var host = Environment.GetEnvironmentVariable("REDIS_HOST");
